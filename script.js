06a8a038754ac8310df512613089fd3599f2744f
Davel <davel.reymundo@gmail.com>
2023-08-21 13:37:14 -0500

    style: shadow header in scroll

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-let navBarMobile = document.querySelector(".menu-mobile");
 const header = document.querySelector(".menu-pc");
+let navBarMobile = document.querySelector(".menu-mobile");
 
 function showMenu() {
     navBarMobile.classList.toggle("menu-mobile--open");    
 }
 
 
-// Agrega un evento de desplazamiento (scroll)
+// Agrega un evento de desplazamiento scroll para header
 window.addEventListener("scroll", () => {
-  // Verifica la posición de desplazamiento vertical
   if (window.scrollY > 0) {
-    // Agrega una clase CSS al encabezado para aplicar la sombra
     header.classList.add("scrolled");
   } else {
-    // Remueve la clase si la posición de desplazamiento es cero
     header.classList.remove("scrolled");
   }
 });
